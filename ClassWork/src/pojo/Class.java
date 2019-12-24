32e7a4e177538b4bf430af0fd9b366d6869f4e99
Ji-Lei <1042318800@qq.com>
2019-12-24 14:12:32 +0800

    课堂作业

---
+++ ClassWork/src/pojo/Class.java
@@ -0,0 +1,19 @@
+package pojo;
+
+public class Class {
+	private int classno;
+	private String classname;
+	public int getClassno() {
+		return classno;
+	}
+	public void setClassno(int classno) {
+		this.classno = classno;
+	}
+	public String getClassname() {
+		return classname;
+	}
+	public void setClassname(String classname) {
+		this.classname = classname;
+	}
+	
+}
---
+++ ClassWork/src/pojo/Phone.java
@@ -0,0 +1,26 @@
+package pojo;
+
+public class Phone {
+	private int pno;
+	private String phone;
+	private int cno;
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getCno() {
+		return cno;
+	}
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+	
+}
